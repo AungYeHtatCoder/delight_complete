465971f25f001b29fa1acabd64a2b9d96aa690c2
AungYeHtatCoder <aungyehtatcoder2021@gmail.com>
2023-08-18 18:24:47 +0630

    reparing noti blade

--- resources/views/admin/event_noti/index.blade.php
+++ resources/views/admin/event_noti/index.blade.php
@@ -139,7 +139,7 @@
 @section('scripts')
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 @parent
-@if(auth()->user()->is_admin)
+@if(auth()->user()->is_user)
 <script>
 $(function() {
  function sendMarkRequest(id = null) {
